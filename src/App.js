814c3570e92e030e95100400504579c2c32e3e7c
Trelobill <vasilis_gagakis@yahoo.gr>
2024-06-20 08:17:43 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My portfolio!
+          My portfolio!!
         </p>
       </header>
     </div>
